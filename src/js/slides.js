39df96dd0dd7104ba4cc577bc17cdc4c21e45924
sapphi-red <green@sapphi.red>
2020-04-13 18:53:30 +0900

    Update: スライドの戻る機能

--- src/js/slides.js
+++ src/js/slides.js
@@ -14,22 +14,44 @@ export const checkImgLoading = $slide => {
   }, { once: true })
 }
 
+const getLeftOrRight = ($slide, xTarget) => {
+  const { left, right } = $slide.getBoundingClientRect()
+  const xMiddle = (left + right) / 2
+
+  return xTarget < xMiddle ? 'left' : 'right'
+}
+
 export const setupSlides = () => {
   const $$slides = document.querySelectorAll(".slides")
   for (const $slides of $$slides) {
     $slides.dataset.now = 1
     $slides.dataset.length = $slides.children.length
 
-    $slides.addEventListener('click', () => {
+    $slides.addEventListener('click', e => {
       const $selected = $slides.querySelector('[is-selected]')
       $selected.removeAttribute('is-selected')
-      if ($selected.nextElementSibling) {
-        $selected.nextElementSibling.setAttribute('is-selected', 'is-selected')
-        checkImgLoading($selected.nextElementSibling)
-        $slides.dataset.now++
-      } else {
-        $slides.firstChild.setAttribute('is-selected', 'is-selected')
-        $slides.dataset.now = 1
+
+      const place = getLeftOrRight($slides, e.clientX)
+
+      if (place === 'right') {
+        if ($selected.nextElementSibling) {
+          $selected.nextElementSibling.setAttribute('is-selected', 'is-selected')
+          checkImgLoading($selected.nextElementSibling)
+          $slides.dataset.now++
+        } else {
+          $slides.firstElementChild.setAttribute('is-selected', 'is-selected')
+          $slides.dataset.now = 1
+        }
+      } else if (place === 'left') {
+        if ($selected.previousElementSibling) {
+          $selected.previousElementSibling.setAttribute('is-selected', 'is-selected')
+          checkImgLoading($selected.previousElementSibling)
+          $slides.dataset.now--
+        } else {
+          $slides.lastElementChild.setAttribute('is-selected', 'is-selected')
+          checkImgLoading($slides.lastElementChild)
+          $slides.dataset.now = $slides.childElementCount
+        }
       }
     })
   }
