b4f5da569ebaae1970f2674d553afbd549ba2c18
sapphi-red <green@sapphi.red>
2020-04-13 17:04:25 +0900

    Update: スライドのローディング

--- src/js/slides.js
+++ src/js/slides.js
@@ -1,3 +1,19 @@
+const checkImgLoading = $slide => {
+  if ($slide.dataset.loaded) return
+
+  const $img = $slide.querySelector('img')
+  if ($img.complete) {
+    $slide.dataset.loaded = true
+    return
+  }
+
+  $slide.dataset.loaded = false
+
+  $img.addEventListener('load', () => {
+    $slide.dataset.loaded = true
+  }, { once: true })
+}
+
 export const setupSlides = () => {
   const $$slides = document.querySelectorAll(".slides")
   for (const $slides of $$slides) {
@@ -9,6 +25,7 @@ export const setupSlides = () => {
       $selected.removeAttribute('is-selected')
       if ($selected.nextElementSibling) {
         $selected.nextElementSibling.setAttribute('is-selected', 'is-selected')
+        checkImgLoading($selected.nextElementSibling)
         $slides.dataset.now++
       } else {
         $slides.firstChild.setAttribute('is-selected', 'is-selected')
